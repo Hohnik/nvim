1fc4619a8d34d5831e0139850c7e447c6e4a6857
Flamewiing <niklas-hohn@t-online.de>
2024-01-17 23:46:54 +0100

    DailyCommit 2024/01/17 23:46

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,10 +1,14 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  config = function()
-    require("lualine").setup({
-      options = {
-        theme = "auto"
-      }
-    })
-  end
+	"nvim-lualine/lualine.nvim",
+	config = function()
+		require("lualine").setup({
+			sections = {
+				lualine_b = { "branch" },
+				lualine_c = { "buffers" },
+				lualine_x = {},
+				lualine_y = { "diff" },
+				lualine_z = { "os.date('%H:%M')" },
+			},
+		})
+	end,
 }
